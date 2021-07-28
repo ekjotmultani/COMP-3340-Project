8b523ca1fcb0bf8e4c44166ed27e5c572505404e
ekjotmultani <ekjotmultani@gmail.com>
2021-07-28 04:03:16 -0400

    fixed weird sublime text artifacts in login files

--- login/dashboard.php
+++ login/dashboard.php
@@ -6,7 +6,6 @@ include("auth_session.php");
 <head>
     <meta charset="utf-8">
     <title>Dashboard - Client area</title>
-<<<<<<< HEAD
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -28,16 +27,11 @@ include("auth_session.php");
     require('db.php');
     require_once ("../php/header.php");
 
+    
     ?>
     
     <div class="form">
 
-=======
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-    <div class="form">
->>>>>>> cc215af324de6e19782e944ba9937b19702b8285
         <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
         <p>You are in user dashboard page.</p>
         <p><a href="logout.php">Logout</a></p>
